e3e87f10ed60c802c2876faaabea2893d926e06a
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-09 12:36:13 +0100

    Added a simple foreach that writes all the collections but without the respective id

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -5,17 +5,22 @@ namespace IntCollections
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            List<int> list1 = new List<int>(){1, 10, -30, 10, -5};
-            Stack<int> stack1 = new Stack<int>(){1, 10, -30, 10, -5};
-            Queue<int> queue1 = new Queue<int>(){1, 10, -30, 10, -5};
-            HashSet<int> hashSet1 = new HashSet<int>(){1, 10, -30, 10, -5};
-            var[] diflists = new var[] {list1, stack1, queue1, hashSet1};
-            foreach (var dif in diflists)
+            List<int> list1 = new List<int> { 1, 10, -30, 10, -5 };
+            Stack<int> stack1 = new Stack<int>(new int[] { 1, 10, -30, 10, -5 });
+            Queue<int> queue1 = new Queue<int>(new int[] { 1, 10, -30, 10, -5 });
+            HashSet<int> hashSet1 = new HashSet<int> { 1, 10, -30, 10, -5 };
+
+            IEnumerable<int>[] diflists = new IEnumerable<int>[] { list1, stack1, queue1, hashSet1 };
+
+            foreach (var collection in diflists)
             {
-                Console.WriteLine("egege" + dif.ToString());
+                Console.WriteLine("Collection:");
+                foreach (int item in collection)
+                {
+                    Console.WriteLine(item);
+                }
             }
         }
     }
