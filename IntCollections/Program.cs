10fffbf464976c8dc8d27c8333c86bdb95bf7246
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-09 12:00:11 +0100

    Changed dotnet version and added all the types of collections required to the code, still not able to print them like in the exercise

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> list1 = new List<int>(){1, 10, -30, 10, -5};
+            Stack<int> stack1 = new Stack<int>(){1, 10, -30, 10, -5};
+            Queue<int> queue1 = new Queue<int>(){1, 10, -30, 10, -5};
+            HashSet<int> hashSet1 = new HashSet<int>(){1, 10, -30, 10, -5};
+            var[] diflists = new var[] {list1, stack1, queue1, hashSet1};
+            foreach (var dif in diflists)
+            {
+                Console.WriteLine("egege" + dif.ToString());
+            }
         }
     }
 }
