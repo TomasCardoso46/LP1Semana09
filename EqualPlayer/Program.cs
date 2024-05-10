57f984012c7d6074810508a40c0e27f45f4dea87
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-10 15:30:57 +0100

    Added the exercise 3 code as the template for EqualPlayer

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -9,4 +9,47 @@ namespace EqualPlayer
             Console.WriteLine("Hello, World!");
         }
     }
+    public enum PlayerClass
+    {
+        Tank,
+        Fighter,
+        Slayer,
+        Mage,
+        Controller,
+        Marksmen
+    }
+
+    public class Player
+    {
+        public PlayerClass PClass { get;}
+        public string Name {get;}
+
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass = pClass;
+            Name = name;    
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Player other = (Player)obj;
+            return PClass == other.PClass && Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + PClass.GetHashCode();
+                hash = hash * 23 + (Name != null ? Name.GetHashCode() : 0);
+                return hash;
+            }
+        }
+    }
 }
