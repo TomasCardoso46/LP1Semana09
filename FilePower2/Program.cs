dadbedf3f793d5904fe0c01432b386eae22c5bdc
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-10 15:58:46 +0100

    Made the required changes for point 2

--- FilePower2/Program.cs
+++ FilePower2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace FilePower1
@@ -8,49 +7,31 @@ namespace FilePower1
     {
         static void Main(string[] args)
         {
+           
             if (args.Length == 0)
             {
                 Console.WriteLine("Por favor, forneça o nome do arquivo como argumento.");
                 return;
             }
 
-            string fileName = args[0];
-            Queue<string> stringsQueue = new Queue<string>();
+            string fileName = args[0]; 
 
             Console.WriteLine("Digite as strings (pressione Enter após cada uma):");
 
-           
-            while (true)
-            {
-                string input = Console.ReadLine();
-                if (input == "") 
-                    break;
-                else
-                    stringsQueue.Enqueue(input); 
-            }
-
             
-            WriteToFile(fileName, stringsQueue);
-
-            Console.WriteLine("Strings foram salvas no arquivo com sucesso.");
-        }
-
-        static void WriteToFile(string fileName, Queue<string> stringsQueue)
-        {
-            try
+            using (StreamWriter writer = new StreamWriter(fileName))
             {
-                using (StreamWriter writer = new StreamWriter(fileName))
+                while (true)
                 {
-                    foreach (string str in stringsQueue)
-                    {
-                        writer.WriteLine(str);
-                    }
+                    string input = Console.ReadLine(); 
+                    if (input == "") 
+                        break;
+                    else
+                        writer.WriteLine(input); 
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ocorreu um erro ao escrever no arquivo: {ex.Message}");
-            }
+
+            Console.WriteLine("Strings foram salvas no arquivo com sucesso.");
         }
     }
 }
